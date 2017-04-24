9cf84ca24a16ce56c1cafd15989b57db5d857a34
luxiaok <me@luxiaok.com>
2017-04-24 22:51:26 +0800

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ config = {
     'db': {
         'host': '127.0.0.1',
         'port': 3306,
-        'db': 'torweb',
+        'db': 'dns',
         'user': 'test',
         'passwd': 'test',
         'charset': 'utf8'
@@ -35,8 +35,8 @@ config = {
         static_url_prefix = '/static/',
         xsrf_cookies = False,
         cookie_secret = "db884468559f4c432bf1c1775f3dc9da",
-        cookie_name = '_ksid',
-        session_prefix = "_k_session_",
+        cookie_name = '_knsid',
+        session_prefix = "_k_dns_session_",
         session_expires = 7200,
         login_url = "/user/login",
         default_lang = "en_US",
