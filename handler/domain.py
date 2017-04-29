06ab05decd908c13e5fd8aaef58fd7addba004c5
luxiaok <me@luxiaok.com>
2017-04-29 23:28:42 +0800

    Improve model oder logic

--- handler/domain.py
+++ handler/domain.py
@@ -33,6 +33,6 @@ class RecordHandler(BaseHandler):
     @Auth
     def get(self):
         zone = self.get_argument('zone')
-        data = self.db.query(Record).filter_by(zone=zone).all()
+        data = self.db.query(Record).filter_by(zone=zone).order_by('host,type,data').all()
         status = {1: u'<span style="color:green">已启用</span>', 2: u'<span style="color:red">暂停解析</span>'}
         self.render('domain/record.html',data=data,status=status,zone=zone)
