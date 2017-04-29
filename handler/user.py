e0864443737190b9c2cf526e4a9f4ceb11ea1096
luxiaok <me@luxiaok.com>
2017-04-29 17:46:46 +0800

    Improve Login logic

--- handler/user.py
+++ handler/user.py
@@ -4,6 +4,7 @@
 
 from BaseHandler import BaseHandler
 from tornado.web import authenticated as Auth
+from model.models import User, or_, and_
 
 class LoginHandler(BaseHandler):
 
@@ -17,7 +18,25 @@ class LoginHandler(BaseHandler):
         username = self.get_argument("username", None)
         password = self.get_argument("password", None)
         remember = self.get_argument("remember", "no")
-        #self.create_session(self,data,remember)
+        profile = self.db.query(User).filter(or_(User.username==username,User.email==username),and_(User.status=='1')).first()
+        if profile:
+            password_hash = self.md5(password+profile.password_key)
+            if password_hash != profile.password:
+                return self.jsonReturn({'code':-1,'msg':u'用户名或密码错误'})
+            session_data = {
+                'uid': profile.id,
+                'username': profile.username,
+                'email': profile.email,
+                'nickname': profile.nickname,
+                'login_time': profile.login_time,
+                'login_ip': profile.login_ip,
+                'login_location': profile.login_location,
+            }
+            self.create_session(self, session_data, remember)
+            return self.jsonReturn({'code': 0, 'msg': u'Login Successful'})
+        else:
+            return self.jsonReturn({'code': -1, 'msg': u'用户名或密码错误'})
+
 
     def create_session(self,data,remember):
         sid = self.session.gen_session_id()
