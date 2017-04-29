80288aed953a0f3bb21630a50913bd2b36774fd0
luxiaok <me@luxiaok.com>
2017-04-29 15:06:51 +0800

    Add blank Handler

--- handler/index.py
+++ handler/index.py
@@ -12,3 +12,8 @@ class IndexHandler(BaseHandler):
     def get(self):
         #self.log.info('Hell,Index page!') # Log Test
         self.render('index/index.html')
+
+class BlankHandler(BaseHandler):
+    # @Auth
+    def get(self):
+        self.render('index/blank.html')
