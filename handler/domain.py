185258aeb63373bb3c47a133239019a64fb557af
luxiaok <me@luxiaok.com>
2017-04-30 14:37:40 +0800

    Update  record default value

--- handler/domain.py
+++ handler/domain.py
@@ -33,7 +33,12 @@ class GroupHandler(BaseHandler):
 class RecordHandler(BaseHandler):
     @Auth
     def get(self):
-        zone = self.get_argument('zone')
-        data = self.db.query(Record).filter_by(zone=zone).order_by(Record.host,Record.type,Record.data).all()
+        zone = self.get_argument('zone',None)
+        if zone:
+            data = self.db.query(Record).filter_by(zone=zone).order_by(Record.host,Record.type,Record.data).all()
+        else:
+            zone = u'请选择域名'
+            data = []
         status = {1: u'<span style="color:green">已启用</span>', 2: u'<span style="color:red">暂停解析</span>'}
         self.render('domain/record.html',data=data,status=status,zone=zone)
+
