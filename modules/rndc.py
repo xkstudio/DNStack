af7e1e73349d62649f5a8790d91bde88615fe91e
luxiaok <me@luxiaok.com>
2017-05-01 20:53:10 +0800

    Add get_status

--- modules/rndc.py
+++ modules/rndc.py
@@ -4,12 +4,26 @@
 
 class rndc:
 
-    def __init__(self):
+    def __init__(self,host,port,algo,secret):
         try:
             from isc.rndc import rndc as isc_rndc
         except:
             print 'Not Found ISC Module'
+            isc_rndc = None
+        self.rndc = isc_rndc((host,int(port)),algo,secret)
+        self.call = self.rndc.call
+
+    def get_status(self):
+        _st = self.call('status')
+        status = _st['text'].split('\n')
+        return status
+
 
 
 if __name__ == '__main__':
-    r = rndc()
+    host = '127.0.0.1'
+    port = 953
+    algo = 'md5'
+    secret = 'kzIcztY4+xH0Px2SrsZyGQ=='
+    r = rndc(host,port,algo,secret)
+    print r.get_status()
