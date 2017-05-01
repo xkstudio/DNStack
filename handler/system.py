e0bef594f4c53dae626751d4279dedf58c1a91f9
luxiaok <me@luxiaok.com>
2017-05-01 11:05:23 +0800

    Add system handler

---
+++ handler/system.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# Powered By KK Studio
+
+from BaseHandler import BaseHandler
+from tornado.web import authenticated as Auth
+from model.models import Options
+
+class StateHandler(BaseHandler):
+
+    @Auth
+    def get(self):
+        self.render('system/state.html')
+
