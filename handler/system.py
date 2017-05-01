948130863a1d1152ffb738618349364bc016eb93
luxiaok <me@luxiaok.com>
2017-05-01 11:46:02 +0800

    Add save system settings interface

--- handler/system.py
+++ handler/system.py
@@ -25,3 +25,21 @@ class SettingsHandler(BaseHandler):
             data[i.name] = {'id':i.id,'name':i.name,'value':i.value,'default':i.default_value}
         self.render('system/settings.html',data=data)
 
+
+    @Auth
+    def post(self):
+        data = dict(
+            primary_ns=self.get_argument('primary_ns') or None,
+            second_ns = self.get_argument('second_ns') or None,
+            resp_person = self.get_argument('resp_person') or None,
+            rndc_host = self.get_argument('rndc_host') or None,
+            rndc_port = self.get_argument('rndc_port') or None,
+            rndc_algo = self.get_argument('rndc_algo') or None,
+            rndc_secret = self.get_argument('rndc_secret') or None
+        )
+        for name in data:
+            self.db.query(Options).filter_by(name=name).update({'value':data[name],'update_time': self.time})
+        self.db.commit()
+        return self.jsonReturn({'code': 0, 'msg': 'Success'})
+
+
